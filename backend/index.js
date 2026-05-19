5b91b85b066bc8484d812c6528c7f11106480aac
java73 <dakshsingh814@gmail.com>
2026-05-19 12:49:55 +0530

    fixed cors

--- backend/index.js
+++ backend/index.js
@@ -3,42 +3,46 @@ import dotenv from "dotenv";
 import mongoose from "mongoose";
 import cors from "cors";
 
+import userRoutes from "./routes/userRoutes.js";
+import complaintRoutes from "./routes/complaintRoutes.js";
+
 dotenv.config();
 
 const app = express();
 
+app.use(cors());
+
 app.use(express.json());
 
-/* IMPORTANT CORS FIX */
-app.use(
-  cors({
-    origin: "*",
-    credentials: true,
-  })
-);
-
-/* ROUTES */
-import authRoutes from "./routes/authRoutes.js";
-import complaintRoutes from "./routes/complaintRoutes.js";
-
-app.use("/api/auth", authRoutes);
-app.use("/api/complaints", complaintRoutes);
-
-/* TEST ROUTE */
-app.get("/", (req, res) => {
-  res.send("API Running...");
-});
-
-/* MONGODB CONNECT */
 mongoose
   .connect(process.env.MONGO_URI)
   .then(() => {
+
     console.log("MongoDB Connected");
 
-    app.listen(process.env.PORT || 5000, () => {
-      console.log("Server Running");
-    });
   })
-  .catch((err) => {
-    console.log(err);
+  .catch((error) => {
+
+    console.log(error);
+
   });
+
+app.get("/", (req, res) => {
+
+  res.send("API Running");
+
+});
+
+app.use("/api/users", userRoutes);
+
+app.use("/api/complaints", complaintRoutes);
+
+const PORT = process.env.PORT || 5000;
+
+app.listen(PORT, () => {
+
+  console.log(
+    `Server Running On Port ${PORT}`
+  );
+
+});
