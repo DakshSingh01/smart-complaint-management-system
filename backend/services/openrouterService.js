ec32d8a477b1a14fcf2b60c54b92d1aa167fff06
java73 <dakshsingh814@gmail.com>
2026-05-19 16:33:54 +0530

    fixed ai production

--- backend/services/openrouterService.js
+++ backend/services/openrouterService.js
@@ -1,7 +1,9 @@
 import axios from "axios";
 
 export const analyzeComplaintAI = async (description) => {
+
   try {
+
     const prompt = `
 Analyze this complaint carefully:
 
@@ -17,30 +19,36 @@ Automated Response:
 `;
 
     const response = await axios.post(
+
       "https://openrouter.ai/api/v1/chat/completions",
+
       {
         model: "openai/gpt-4o-mini",
+
         messages: [
           {
             role: "user",
-            content: prompt
-          }
-        ]
+            content: prompt,
+          },
+        ],
       },
+
       {
         headers: {
           Authorization: `Bearer ${process.env.OPENROUTER_API_KEY}`,
           "Content-Type": "application/json",
-          "HTTP-Referer": "http://localhost:5000",
-          "X-Title": "Complaint Management System"
-        }
+        },
       }
     );
 
     return response.data.choices[0].message.content;
-  } catch (error) {
-    console.log(error.response?.data || error.message);
 
-    return "AI analysis unavailable";
+  } catch (error) {
+
+    console.log(
+      error.response?.data || error.message
+    );
+
+    throw new Error("AI Analysis Failed");
   }
 };
