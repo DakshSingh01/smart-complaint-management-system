47234698ff8833624da57949343834eb7668753e
java73 <dakshsingh814@gmail.com>
2026-05-19 14:34:45 +0530

    final stable fix

--- frontend/smart-complaint-management/src/main.jsx
+++ frontend/smart-complaint-management/src/main.jsx
@@ -1,7 +1,7 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
 
-import { BrowserRouter } from "react-router-dom";
+import { HashRouter } from "react-router-dom";
 
 import App from "./App";
 
@@ -12,9 +12,9 @@ ReactDOM.createRoot(
 ).render(
   <React.StrictMode>
 
-    <BrowserRouter>
+    <HashRouter>
       <App />
-    </BrowserRouter>
+    </HashRouter>
 
   </React.StrictMode>
 );
