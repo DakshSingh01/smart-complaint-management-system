08939554b204d6aeffe7370a4d6d521cd67acd96
java73 <dakshsingh814@gmail.com>
2026-05-19 12:46:02 +0530

    fixed frontend api

--- frontend/smart-complaint-management/src/api/axios.js
+++ frontend/smart-complaint-management/src/api/axios.js
@@ -1,17 +1,10 @@
 import axios from "axios";
 
 const API = axios.create({
-  baseURL: "https://smart-complaint-management-system-yx9f.onrender.com/api"
-});
 
-API.interceptors.request.use((req) => {
-  const token = localStorage.getItem("token");
+  baseURL:
+    "https://smart-complaint-management-system-yx9f.onrender.com/api",
 
-  if (token) {
-    req.headers.Authorization = `Bearer ${token}`;
-  }
-
-  return req;
 });
 
 export default API;
